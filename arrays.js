088b608447654761ea448673888032149d587211
nategarvison <ngarvison@gmail.com>
2017-08-24 02:30:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
-  array;
+  array = array.slice(0, array.length - 1)
   return array;
 }
