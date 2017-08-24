65615ad359fdaca06c89592fd236f3ada09f9ae0
nategarvison <ngarvison@gmail.com>
2017-08-24 02:22:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,16 +32,16 @@ function removeElementFromBeginningOfArray(array) {
   return array;
 
 }
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop();
-  return array;
-}
-
-function destructivelyRemoveElementFromEndOfArray(array) {
-
-}
-
-function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
-  return array;
-}
+// function destructivelyRemoveElementFromEndOfArray(array) {
+//   array.pop();
+//   return array;
+// }
+//
+// function destructivelyRemoveElementFromEndOfArray(array) {
+//
+// }
+//
+// function removeElementFromEndOfArray(array) {
+//   array.slice(0, array.length - 1)
+//   return array;
+// }
